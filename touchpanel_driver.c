b9206b9c866a3b4c3a19e8be75778c44e7a00f3a
B10213147 <B10213147@yuntech.org.tw>
2016-10-13 09:24:45 +0800

    Detect DV signel
    TTP229-BSF doesnt use I2C interface

--- touchpanel_driver.c
+++ touchpanel_driver.c
@@ -8,34 +8,38 @@
 #include "touchpanel_driver.h"
 #include "rtos.h"
 #include "TM4C123GH6PM.h"
-#include "driverlib/i2c.h"
 #include "driverlib/gpio.h"
 #include "driverlib/sysctl.h"
-#include "driverlib/pin_map.h"
+
+void data_valid(void);
 
 void touchpanel_driver(void){
-
-
+	GPIOIntEnable(GPIOD_BASE, GPIO_INT_PIN_1);
 }
 
 void touchpanel_driver_init(void){
-	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
-	SysCtlPeripheralEnable(SYSCTL_PERIPH_I2C1);
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);
 
 	//
 	// Check if the peripheral access is enabled.
 	//
-	while(!SysCtlPeripheralReady(SYSCTL_PERIPH_I2C1))
+	while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOD))
 	{
 	}
 
-	//Configure PA6, PA7 Pin as I2CSCL, I2CSDA
-	GPIOPinConfigure(GPIO_PA6_I2C1SCL);
-	GPIOPinConfigure(GPIO_PA7_I2C1SDA);
-	GPIOPinTypeI2CSCL(GPIOA_BASE, GPIO_PIN_6);
-	GPIOPinTypeI2C(GPIOA_BASE, GPIO_PIN_7);
+	// SCL
+	GPIOPinTypeGPIOOutput(GPIOD_BASE, GPIO_PIN_0);
+	GPIOPinWrite(GPIOD_BASE, GPIO_PIN_0, GPIO_PIN_0);
+	// SDO
+	GPIOPinTypeGPIOInput(GPIOD_BASE, GPIO_PIN_1);
 
-	uint32_t sysctl = SysCtlClockGet();
-	I2CMasterInitExpClk(I2C1_BASE, sysctl, false);
-	I2CMasterEnable(I2C1_BASE);
+	GPIOIntTypeSet(GPIOD_BASE, GPIO_PIN_1, GPIO_FALLING_EDGE);
+	GPIOIntRegister(GPIOD_BASE, data_valid);
+	GPIOIntEnable(GPIOD_BASE, GPIO_INT_PIN_1);
+}
+
+void data_valid(void){
+	GPIOIntClear(GPIOD_BASE, GPIO_INT_PIN_1);
+	GPIOIntDisable(GPIOD_BASE, GPIO_INT_PIN_1);
+	touchpanel_driver();
 }
